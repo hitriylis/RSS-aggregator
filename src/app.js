f3a4508d3ad73c8680af24ac4e4d5cddcbcac216
Andrei <avlugovov@gmail.com>
2023-08-22 19:42:52 +0400

    Update content by step 6

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import onChange from 'on-change';
 import * as yup from 'yup';
 import { string } from 'yup';
 import _ from 'lodash';
-import render from './view';
+import { render, handleProcessState } from './view';
 import parser from './parser';
 
 yup.setLocale({
@@ -15,14 +15,38 @@ const urlSchema = (string().url());
 
 const state = {
   urls: [],
+  isValid: '',
   error: '',
-  content: [],
+  feeds: [],
+  posts: [],
+  uiState: {
+    watchedPosts: [],
+  },
+  modalPost: '',
 };
 
 let timerId = '';
 
-const watchedState = onChange(state, () => {
-  render(state);
+const watchedState = onChange(state, (path) => {
+  switch (path) {
+    case 'isValid':
+      handleProcessState(state);
+      break;
+    case 'feeds':
+      render(state, 'feeds');
+      break;
+    case 'posts':
+      render(state, 'posts');
+      break;
+    case 'uiState.watchedPosts':
+      render(state, 'posts');
+      break;
+    case 'modalPost':
+      render(state, 'modalPost');
+      break;
+    default:
+      break;
+  }
 });
 
 const responseDocument = (url, doc, initialState) => {
@@ -30,29 +54,33 @@ const responseDocument = (url, doc, initialState) => {
   const feedDescription = doc.body.querySelector('description').textContent;
   const posts = doc.querySelectorAll('item');
   const feedUrl = url;
-  const feedPosts = [];
 
   if (!initialState.urls.includes(url)) {
+    const feedsCount = initialState.feeds.length;
+    const postsCount = feedsCount * 100;
+    const feedId = postsCount;
     initialState.urls.push(url);
-    watchedState.content.push({
-      feedTitle, feedDescription, feedPosts, feedUrl,
+    watchedState.isValid = 'done';
+    watchedState.feeds.push({
+      feedTitle, feedDescription, feedId, feedUrl,
     });
   }
 
-  const findElement = (obj) => (_.get(obj, 'feedUrl') === url);
+  const findFeed = (obj) => (_.get(obj, 'feedUrl') === url);
+  const currentFeed = initialState.feeds.find(findFeed);
+  let postId = currentFeed.feedId;
 
-  const currentFeed = initialState.content.find(findElement);
+  state.posts = [];
 
-  currentFeed.feedPosts = [];
   posts.forEach((post) => {
     const postTitle = post.querySelector('title').textContent;
     const postDescription = post.querySelector('description').textContent;
     const linkElement = post.querySelector('link');
     const postLink = linkElement.nextSibling.textContent.trim();
-    currentFeed.feedPosts.push({
-      postTitle, postDescription, postLink,
+    watchedState.posts.push({
+      postTitle, postDescription, postLink, postId,
     });
-    render(initialState);
+    postId += 1;
   });
 };
 
@@ -65,33 +93,9 @@ const postsSelection = (url) => {
       responseDocument(url, doc, state);
     })
     .catch(() => {
-      watchedState.error = 'parseError';
+      state.error = 'parseError';
+      watchedState.isValid = 'error';
     });
-};
-
-const checkFeed = (url) => {
-  fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`)
-    .then((response) => response.json())
-    .then((data) => data.contents)
-    .then((text) => parser(text))
-    .then((doc) => {
-      const posts = doc.querySelectorAll('item');
-      // const postsCount = posts.length;
-      const findElement = (obj) => (_.get(obj, 'feedUrl') === url);
-      const currentFeed = state.content.find(findElement);
-      currentFeed.feedPosts = [];
-      posts.forEach((post) => {
-        const postTitle = post.querySelector('title').textContent;
-        const postDescription = post.querySelector('description').textContent;
-        const linkElement = post.querySelector('link');
-        const postLink = linkElement.nextSibling.textContent.trim();
-        currentFeed.feedPosts.push({
-          postTitle, postDescription, postLink,
-        });
-        render(state);
-      });
-    })
-    .catch((error) => console.log(error));
 };
 
 const app = () => {
@@ -100,27 +104,38 @@ const app = () => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const urlName = formData.get('url');
-    urlSchema.validate(urlName)
-      .then((response) => {
-        if (state.urls.includes(response)) {
-          watchedState.error = 'validate.errors.existingUrl';
-        } else {
-          state.error = '';
-          postsSelection(urlName);
-        }
-      })
-      .then(() => {
+    urlSchema.validate(urlName).then((response) => {
+      if (state.urls.includes(response)) {
+        state.error = 'validate.errors.urlRepeatable';
+        watchedState.isValid = 'error';
+      } else {
+        state.error = '';
+        watchedState.isValid = 'sending';
+        postsSelection(urlName);
         clearTimeout(timerId);
         timerId = setTimeout(function innerFunc() {
-          state.content.forEach(({ feedUrl }) => {
-            checkFeed(feedUrl);
+          state.feeds.forEach(({ feedUrl }) => {
+            postsSelection(feedUrl);
           });
           timerId = setTimeout(innerFunc, 5000);
         }, 5000);
-      })
+      }
+    })
       .catch((error) => {
-        watchedState.error = error.message;
+        state.error = error.message;
+        watchedState.isValid = 'error';
       });
+  });
+
+  const postsList = document.querySelector('.list-group');
+  postsList.addEventListener('click', (e) => {
+    const button = e.target;
+    const link = button.previousSibling;
+    const watchedPostLink = link.getAttribute('href');
+    const watchedPostId = button.getAttribute('data-id');
+    const findPost = (obj) => (_.get(obj, 'postId') === Number(watchedPostId));
+    watchedState.modalPost = state.posts.find(findPost);
+    watchedState.uiState.watchedPosts.push(watchedPostLink);
   });
 };
 
